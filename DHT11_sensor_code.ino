c76d0f3615aef0adca0015914bfd3cae31a5568e
Archer743 <61616845+Archer743@users.noreply.github.com>
2022-03-11 14:23:39 +0200

    Create DHT11_sensor_code.ino
    Код за температурен сензор и влажност - DHT11, изпозваща библиотеката DHT.h на Adafruit version 1.4.3

---
+++ DHT11_sensor_code.ino
@@ -0,0 +1,28 @@
+#include <DHT.h>
+#define Type DHT11
+
+int sensePin=7; //pin
+DHT HT(sensePin, Type); //object HT- Humanity sensor
+float humidity;
+float tempC;
+int set_time = 500;
+int dt=1000;
+ 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  HT.begin();
+  delay(set_time);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  humidity = HT.readHumidity();
+  tempC = HT.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.print("\n");
+  delay(1000);
+}
