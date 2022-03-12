c98e9ee23cfde2a00a874f3cf7867a038d8930c0
Archer743 <61616845+Archer743@users.noreply.github.com>
2022-03-12 10:43:42 +0200

    Update safespace1ultrasonic.ino

--- safespace1ultrasonic.ino
+++ safespace1ultrasonic.ino
@@ -1,27 +1,27 @@
-int const trigPin = 10;
-int const echoPin = 9;
-int const buzzPin = 2;
+int const trigPin = 11;
+int const echoPin = 10;
+int const buzzPin = 4;
 
-void setup()
+void setupsonic() //ultrasonic
 {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(buzzPin, OUTPUT);
 }
 
-void loop()
+void ultrasonic() //ultrasonic
 {
   int duration, distance; // preobraz. vreme v razst.
-  
+
   digitalWrite(trigPin, HIGH); //output width on trigpin
   delay(1);
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH); //izmerva puls input na echopin
-  
+
   distance = (duration/2) / 29.1; //razst=(vremeto/2)/21.9    /ot datasheet/
-  
-    if (distance <= 75 && distance >= 0) //ako razst e < 1m i poveche ot 0:
+
+    if (distance <= 75 && distance >= 0) //ako razst e < 75cm i poveche ot 0:
     {
       digitalWrite(buzzPin, HIGH); //zvunche
     } 
@@ -29,5 +29,5 @@ void loop()
     {
       digitalWrite(buzzPin, LOW); //nqma zvunche
     }
-    delay(60);
+    delay(60); //shtoto shto ne
 }
