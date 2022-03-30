01bbc3b831a7cb7633725e33d25ada40cce48b1e
Archer743 <61616845+Archer743@users.noreply.github.com>
2022-03-30 20:59:13 +0300

    Update load.py

--- Discord Bot/Data/load.py
+++ Discord Bot/Data/load.py
@@ -3,30 +3,16 @@ from termcolor import colored
 
 
 def load_commands(client, l_coms):
-    print(colored("===================", "red") + "COMMANDS" + colored("========================", "red"))
-
     for filename in os.listdir('./Commands'):
         if filename.endswith('.py'):
             client.load_extension('Commands.{}'.format(filename[:-3]))
-            print(colored("> ", "magenta") + colored("'{}'".format(filename[:-3]), 'green') + colored(" is loaded!", "magenta"))
             l_coms += 1
-    
-    if l_coms == 0: print(colored(text="- None!", color="magenta"))
-    else: print(colored("\n- Loaded Commands: ", "magenta") + colored("{}".format(l_coms), "green"))
 
-
-def load_events(client, l_evs):
-    print(colored("===================", "red") + "EVENTS" + colored("=========================", "red"))
-    
+def load_events(client, l_evs):    
     for filename in os.listdir('./Events'):
         if filename.endswith('.py'):
             client.load_extension('Events.{}'.format(filename[:-3]))
-            print(colored("> ", "magenta") + colored("'{}'".format(filename[:-3]), 'green') + colored(" is loaded!", "magenta"))
             l_evs += 1
-    
-    if l_evs == 0: print(colored(text="None!", color="magenta"))
-    else: print(colored("\n- Loaded Events: ", "magenta") + colored("{}".format(l_evs), "green"))
-
 
 def load(client):
     l_coms = 0  # loaded commands
